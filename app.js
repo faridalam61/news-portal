ba19f0679d42c8a43915715757a1f58e5ca48837
Farid Alam <faridalam62@gmail.com>
2023-03-02 13:01:43 +0600

    HTML updated

--- app.js
+++ app.js
@@ -27,13 +27,20 @@ const createNewsCard = (data)=>{
     data.forEach(news =>{
         console.log(news);
         container.innerHTML += `
-            <div class="flex flex-col lg:flex-row gap-6 rounded border border-slate-800 dark:border-slate-500 bg-white dark:bg-slate-600 mb-6">
-                <div class="w-full lg:w-1/4">
-                    <img src="${news.image_url}" alt="" class="rounded w-full">
+            <div class="flex flex-col lg:flex-row gap-4 rounded-lg border border-slate-800 dark:border-slate-500 bg-white dark:bg-slate-600 mb-6">
+                <div class="w-full lg:w-2/6">
+                    <img src="${news.image_url}" alt="" class="rounded-t-lg lg:rounded-l-lg w-full h-96 lg:h-full">
                 </div>
-                <div class="w-full lg:w-9/12 p-4">
-                    <h2 class="text-2xl font-bold mb-4">${news.title}</h2>
+                <div class="w-full lg:w-4/6 p-4">
+                    <h2 class="text-xl font-bold mb-4">${news.title}</h2>
                     <p>${news.details.slice(0,200)}</p>
+
+                    <div class="flex justify-between mt-6">
+                        <p>lorem</p>
+                        <p>lorem</p>
+                        <p>lorem</p>
+                        <p>lorem</p>
+                    </div>
                 </div>
             </div>
         `;
