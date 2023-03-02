529540392ad6e765961e7ae81491a425269404e4
Farid Alam <faridalam62@gmail.com>
2023-03-02 12:35:25 +0600

    Api added to view categories and news

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('ok')
+// Load news categories
+const getCategories = ()=>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then (categories => displayCategories(categories.data))
+}
+
+// Display loaded categories to DOM
+const displayCategories = (data)=>{
+    const navbar = document.getElementById('nav');
+    data.news_category.forEach(categories => {
+        navbar.innerHTML += `<p href="" onclick = "loadNews('${categories.category_id}')" class="cursor-pointer">${categories.category_name}</p>`
+    })
+}
+
+// Load news from the api
+const loadNews = (id)=>{
+    fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
+    .then(res => res.json())
+    .then(data => createNewsCard(data.data))
+}
+
+// Display loaded news to dom
+const createNewsCard = (data)=>{
+    const container = document.getElementById('all-news');
+    container.innerHTML='';
+    data.forEach(news =>{
+        console.log(news);
+        container.innerHTML += `
+            <div class="flex flex-col lg:flex-row gap-6 rounded border border-slate-800 dark:border-slate-500 bg-white dark:bg-slate-600 mb-6">
+                <div class="w-full lg:w-1/4">
+                    <img src="${news.image_url}" alt="" class="rounded w-full">
+                </div>
+                <div class="w-full lg:w-9/12 p-4">
+                    <h2 class="text-2xl font-bold mb-4">${news.title}</h2>
+                    <p>${news.details.slice(0,200)}</p>
+                </div>
+            </div>
+        `;
+    })
+
+}
